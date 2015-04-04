5e9255128876044413c65d5c17c3e1711e675ec1
kyokomi <kyoko1220adword@gmail.com>
2015-04-04 16:29:38 +0900

    type色々追加

--- server/models/appinfo.go
+++ server/models/appinfo.go
@@ -1,14 +1,43 @@
 package models
 
+type AppCategory string
+
+const (
+	GameCategory  AppCategory = "ゲーム"
+	WebCategory   AppCategory = "Webサービス"
+	StudyCategory AppCategory = "学習"
+)
+
+type PlatformType string
+
+const (
+	IOS     PlatformType = "iOS"
+	Android PlatformType = "Android"
+	Web     PlatformType = "Web"
+)
+
+type LanguageType string
+
+const (
+	GoLang     LanguageType = "Go"
+	Java       LanguageType = "Java"
+	JavaScript LanguageType = "JavaScript"
+	ObjectiveC LanguageType = "Objective-C"
+	Swift      LanguageType = "Swift"
+)
+
 // AppInfo アプリ
 type AppInfo struct {
-	ID        string // アプリID
-	Name      string // アプリ名
-	Title     string // アプリ紹介タイトル
-	Detail    string // アプリ詳細
-	URLs      string // AppStoreのURLとかGitHubのURLとか TODO: 一旦1個あとでリストにする
-	ImageURL  string // 画像のURL（s3?）
-	StarCount int    // スター数
+	ID           string       // アプリID
+	Name         string       // アプリ名
+	Title        string       // アプリ紹介タイトル
+	Detail       string       // アプリ詳細
+	URLs         string       // AppStoreのURLとかGitHubのURLとか TODO: 一旦1個あとでリストにする
+	ImageURL     string       // 画像のURL（s3?）
+	StarCount    int          // スター数
+	Category     AppCategory  // カテゴリ
+	PlatformType PlatformType // プラットフォーム
+	LanguageType LanguageType // プログラミング言語
 }
 
 // URLInfo 各種URL情報
