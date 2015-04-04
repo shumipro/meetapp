76aa4d962e99fdfd3e276e107496508e14d9bc6d
kyokomi <kyoko1220adword@gmail.com>
2015-04-04 15:52:12 +0900

    mockdataを別ファイルに切り分け

---
+++ server/views/mockdata.go
@@ -0,0 +1,44 @@
+package views
+
+import "github.com/shumipro/meetapp/server/models"
+
+// TODO: あとで消す
+
+var mockDataList = []models.AppInfo{
+	{
+		ID:        "001",
+		Name:      "アプリ1",
+		Title:     "1あああああああああ",
+		Detail:    "1いいいいいいいいいいいいいいいいいい\nいいいいいいい",
+		URLs:      "http://google.com",
+		ImageURL:  "https://golang.org/doc/gopher/gopherbw.png",
+		StarCount: 100,
+	},
+	{
+		ID:        "002",
+		Name:      "アプリ2",
+		Title:     "2あああああああああ",
+		Detail:    "2いいいいいいいいいいいいいいいいいい\nいいいいいいい",
+		URLs:      "http://google.com",
+		ImageURL:  "https://golang.org/doc/gopher/gopherbw.png",
+		StarCount: 30,
+	},
+	{
+		ID:        "003",
+		Name:      "アプリ3",
+		Title:     "3あああああああああ",
+		Detail:    "3いいいいいいいいいいいいいいいいいい\nいいいいいいい",
+		URLs:      "http://google.com",
+		ImageURL:  "https://golang.org/doc/gopher/gopherbw.png",
+		StarCount: 20,
+	},
+	{
+		ID:        "004",
+		Name:      "アプリ4",
+		Title:     "4あああああああああ",
+		Detail:    "4いいいいいいいいいいいいいいいいいい\nいいいいいいい",
+		URLs:      "http://google.com",
+		ImageURL:  "https://golang.org/doc/gopher/gopherbw.png",
+		StarCount: 40,
+	},
+}
