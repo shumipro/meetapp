7f76b569e585a363b9a178aaa11a12f4da23347e
tejitak <tejitak@gmail.com>
2015-06-21 12:03:10 +0900

    need app access token

--- server/views/app_join.go
+++ server/views/app_join.go
@@ -35,9 +35,39 @@ func APIAppJoin(ctx context.Context, w http.ResponseWriter, r *http.Request) {
 
 	notification.SendJoin(ctx, user, appInfo)
 
-	// TODO: set AppInfo to DB?
+	// TODO: update AppInfo in DB with a joined flag?
 	// if err := models.AppsInfoTable.Upsert(ctx, appInfo); err != nil {
 	// 	panic(err)
+	// }
+
+	// Facebook notification API
+	// https://developers.facebook.com/docs/graph-api/reference/v2.3/user/notifications
+	// for _, m := range appInfo.Members {
+	// 	u, _ := models.UsersTable.FindID(ctx, m.UserID)
+	// 	if u.FBUser.ID == "" {
+	// 		continue
+	// 	}
+	// 	// check FB account
+	// 	urlTmpl := "https://graph.facebook.com/v2.3/{user-id}/notifications?access_token={access-token}&href={href}&template={message}"
+	// 	// TODO: get access token and href
+	// 	r := strings.NewReplacer("{access-token}", "", "{user-id}", u.FBUser.ID, "{href}", "%2Ftesturl%3Fparam1%3Dvalue1", "{message}", "This+is+a+test+message")
+	// 	fbNotificationURL := r.Replace(urlTmpl)
+	// 	// send notificatinon
+	// 	log.Println(fbNotificationURL)
+	// 	req, err := http.NewRequest("POST", fbNotificationURL, nil)
+	// 	if err != nil {
+	// 		log.Println("ERROR!", err)
+	// 		renderer.JSON(w, 400, err.Error())
+	// 		return
+	// 	}
+	// 	client := &http.Client{}
+	// 	res, err := client.Do(req)
+	// 	if err != nil {
+	// 		log.Println("ERROR!", err)
+	// 		renderer.JSON(w, 400, err.Error())
+	// 		return
+	// 	}
+	// 	defer res.Body.Close()
 	// }
 
 	renderer.JSON(w, 200, appInfo)
